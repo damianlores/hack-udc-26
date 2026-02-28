37f70dbe40988c9bc2faa69c71e381aeba632fa8
damian <d.lores@udc.es>
2026-02-28 06:22:34 +0100

    got interface in main

--- source/interfaz.py
+++ source/interfaz.py
@@ -279,8 +279,8 @@ class MainWindow(QMainWindow):
         self.paginas.addWidget(nueva)
         self.paginas.setCurrentWidget(nueva)
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    w = MainWindow()
-    w.show()
-    sys.exit(app.exec())
+def init_ui():
+     app = QApplication(sys.argv)
+     w = MainWindow()
+     w.show()
+     sys.exit(app.exec())
--- source/main.py
+++ source/main.py
@@ -1,13 +1,7 @@
 import interfaz
 import resources
 import prompt
-from PyQt6.QtWidgets import QApplication
 
 if __name__ == "__main__":
     
-    import sys
-    
-    app = QApplication(sys.argv)
-    w = interfaz.MainWindow()
-    w.show()
-    sys.exit(app.exec())
+    interfaz.init_ui()
