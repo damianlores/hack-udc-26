c28020c0a64bab7950f1989de96e354827020412
damian <d.lores@udc.es>
2026-02-28 03:41:06 +0100

    update test

--- source/test.py
+++ source/test.py
@@ -4,18 +4,18 @@ import time
 
 # --- Ejecución de prueba ---
 if __name__ == "__main__":
-    conn = init_database()
+    conn = database.init_database()
     
     # Simulación de recolección continua (ejecutar 3 veces)
     for _ in range(3):
-        metric_register(conn)
+        database.metric_register(conn)
         time.sleep(1)
     
-    metrics = get_latest_metrics(conn)
+    metrics = database.get_latest_metrics(conn)
     if metrics:
         cpu, ram, procesos = metrics
         print("\nIniciando análisis de IA con los últimos datos...")
-        resultado_ia = analize_resource_usage(cpu, ram, procesos)
+        resultado_ia = prompt.resource_analyze(cpu, ram, procesos)
         print("\n--- Respuesta de IA ---")
         print(resultado_ia)
         
